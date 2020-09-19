37be7eb2af2d875dea67d0d421fc19becfc121ec
MayouKurayami <mkw801021@gmail.com>
2020-09-18 17:56:25 -0700

    Version up to 0.11

--- CrossFader/Bero.CrossFader/CrossFader.cs
+++ CrossFader/Bero.CrossFader/CrossFader.cs
@@ -12,7 +12,7 @@ namespace Bero.CrossFader
 	public class CrossFader : BaseUnityPlugin
 	{
 		public const string PluginName = "CrossFader";
-		public const string Version = "0.10";
+		public const string Version = "0.11";
 		internal static HFlag flags;
 		internal static ChaControl female;
 
